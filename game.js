a43d2179be8d8f1559e030ee9c5fbde3e73285c8
Cppshizoid <alex.krivoschecov@gmail.com>
2022-04-03 02:28:12 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,38 @@
 
+class Snake{
+    constructor(x, y, size) {
+        this.x = x
+        this.y = y
+        this.size = size
+        this.tail = [{x:this.x, y:this.y}]
+        this.rotateX = 0
+        this.rotateY = 1
+
+    }
+
+    move(){
+
+    }
+}
+
+
+var canvas = document.getElementsById("canvas")
+
+var snake = new Snake();
+
+var apple = new Apple();
+
+var canvasContext = canvas.getContext('2d');
+
+window.onload = ()=>{
+    gameLoop();
+}
+
+function gameLoop(){
+    setInterval(show, 1000/15)
+}
+
+function show(){
+    update();
+    draw();
+}
